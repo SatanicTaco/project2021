e45ba1f03686316e4e268f0659a88c805d12a5a6
SatanicTaco <55203131+SatanicTaco@users.noreply.github.com>
2021-03-30 15:50:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,23 @@
 
   <h2>Set water/food level</h2>
     <form action="/action_page.php">
-      <label for="lname">Water level:</label><br>
-      <input type="radio" id="half" name="water" value="half">
+      <label for="waterlvl">Water level:</label><br>
+      <input type="radio" id="half" name="water" value="1">
       <label for="half">Half</label><br>
-      <input type="radio" id="full" name="water" value="full">
+      <input type="radio" id="full" name="water" value="2">
       <label for="full">Full</label><br><br>
-      <label for="lname">Food level:</label><br>
-      <input type="text" id="lname" name="lname"><br><br>
+      <label for="name">Food level: </label><br>
+      <input type="text" name="foodlvl" id="foodlvl"> <br> <br>
       <input type="submit" value="Set">
     </form>
+
+	<?php
+	$val_array = array(0,0,0,0,0,0,0,0);
+	//this php script generate the first page in function of the file
+	for ( $i= 0; $i<8; $i++) {
+		//set the pin's mode to output
+		system("gpio mode ".$i." out");
+	}
 
 </body>
 </html>
